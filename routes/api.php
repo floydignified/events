b6a1a00912911512f63cf980b4ec41c7ed4f52f8
Floyd Matabilas <floydignified@gmail.com>
2020-04-25 10:49:21 +0800

    Floyd > set initial routing

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
+/**
+ * API routes
+ */
+Route::group([
+    "prefix" => "v1",
+], function () {
 
-use Illuminate\Http\Request;
+    /**
+     * Events module
+     */
+    Route::group(["prefix" => "events"], function () {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::get('/', function () {
+            return response()->json(["events" => true]);
+        });
+        Route::get('/create', function () {
+            return response()->json(["events" => true, "create" => true]);
+        });
+    });
 });
