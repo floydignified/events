e299b3d8bfbb48ad40b6773c4686d06ecc6bf197
Floyd Matabilas <floydignified@gmail.com>
2020-04-26 12:27:55 +0800

    Floyd > initialize vue

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app-container">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">Events System</a>
+    </nav>
+    <br />
+    <router-view></router-view>
+  </div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-require('./bootstrap');
+require("./bootstrap");
+window.Vue = require("vue");
+
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import VueAxios from "vue-axios";
+import axios from "axios";
+import { routes } from "./routes";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
+
+const app = new Vue({
+  el: "#app",
+  router: router,
+  render: h => h(App)
+});
