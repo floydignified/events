b76d77ff19e41c102be674f4045e7183b14e5f54
Floyd Matabilas <floydignified@gmail.com>
2020-04-26 12:28:39 +0800

    Floyd > create calendar component

---
+++ resources/js/components/EventCalendar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-100">
+    <FullCalendar
+      ref="calendar"
+      defaultView="dayGridMonth"
+      :plugins="calendarPlugins"
+      :events="events"
+      :displayEventTime="false"
+      eventColor="#28A745"
+      eventTextColor="#FFFF"
+      :buttonText="{ today: 'Today' }"
+    />
+  </div>
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+export default {
+  props: {
+    events: {
+      type: Array,
+      default: []
+    }
+  },
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarPlugins: [dayGridPlugin]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+</style>
